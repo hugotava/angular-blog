54be1313e4e5ed15a166cab087b8d7bd84a2edaf
hugotava <hugotbar@gmail.com>
2023-05-27 10:33:52 -0300

    components createads with styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuWelcomeComponent } from './components/menu-welcome/menu-welcome.component';
+import { MainCardComponent } from './components/main-card/main-card.component';
+import { NewsCardComponent } from './components/news-card/news-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    MenuWelcomeComponent,
+    MainCardComponent,
+    NewsCardComponent,
+    SmallCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/menu-welcome/menu-welcome.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-welcome',
+  templateUrl: './menu-welcome.component.html',
+  styleUrls: ['./menu-welcome.component.css']
+})
+export class MenuWelcomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
